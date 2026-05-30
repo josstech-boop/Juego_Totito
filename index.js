bb284ebbab36e8465731c768c3d079dc76b6fd97
joselyn guitz <joselynguitz@gmail.com>
2026-05-29 21:04:52 -0600

    Se termino proyecto

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 let casillas = document.querySelectorAll('.btn')
-let btnInicio = document.querySelector('.btn-inicio')
 let spanTurno = document.querySelector('.turno')
 let contenedorGanador = document.querySelector('.contenedor-ganador')
 let spanGanador = document.querySelector('.span-ganador')
 let reiniciar = document.querySelector('.btn-reiniciar')
 let spanGandos_x = document.querySelector('.contador_x ')
 let spanGandos_o = document.querySelector('.contador_o ')
+let inicioX = document.querySelector('.btn_X')
+let inicioO = document.querySelector('.btn_O')
 
 let contador = 0
 let casillas_x = []
@@ -15,22 +16,24 @@ let contador_o = 0
 let pintar = []
 let ganados_o = 0
 let ganados_x = 0
-
+let numComparar = 0
 
 const limpiar = () => {
     spanGanador.textContent = ''
     casillas.forEach(casilla => {
-        casilla.disabled = false
+        casilla.disabled = true
         casilla.textContent = ''
         casilla.style.setProperty('background-color', ' rgb(35, 35, 114)')
     })
-    contador = 1
+    contador = 0
     casillas_x = []
     casillas_o = []
     contador_x = 0
     contador_o = 0
     pintar = []
 
+    inicioO.disabled = false
+    inicioX.disabled = false
 }
 
 const pintarCasillas = () => {
@@ -55,7 +58,6 @@ const analizarTablero = (arreglo, turno) => {
 
     for (let i = 0; i < combinacionesGanadoras.length; i++) {
         for (let a = 0; a < arreglo.length; a++) {
-            console.log(turno)
             if (combinacionesGanadoras[i].includes(Number(arreglo[a]))) {
 
                 if (turno == 'O') {
@@ -63,15 +65,11 @@ const analizarTablero = (arreglo, turno) => {
                 } else {
                     contador_x++
                 }
-
                 pintar.push(arreglo[a])
             }
         }
 
         if (contador_o == 3 || contador_x == 3) {
-
-            console.log('Pintar:', pintar)
-
             pintarCasillas()
             return true
         }
@@ -85,16 +83,13 @@ const analizarTablero = (arreglo, turno) => {
 const llenarCasillas = (id_casilla, turno) => {
 
     turno == 'O' ? casillas_o.push(id_casilla) : casillas_x.push(id_casilla)
-
     let analizar_o = casillas_o.length > 2 ? analizarTablero(casillas_o, turno) : false
     let analizar_x = casillas_x.length > 2 ? analizarTablero(casillas_x, turno) : false
 
     if (analizar_o == true) {
         desabilitarCasillas()
-        console.log(casillas_o)
         return true
     } else if (analizar_x == true) {
-        console.log(casillas_x)
         desabilitarCasillas()
         return true
     }
@@ -103,18 +98,29 @@ const llenarCasillas = (id_casilla, turno) => {
 const desabilitarCasillas = () => {
     casillas.forEach(casilla => {
         casilla.disabled = true
-
     })
 }
 
 desabilitarCasillas()
 reiniciar.disabled = true
 
-btnInicio.addEventListener('click', (event) => {
-    console.log('iniciamos')
-    btnInicio.disabled = true
+inicioO.addEventListener('click', (event) => {
+    numComparar = 9
+    inicioO.disabled = true
+    inicioX.disabled = true
     reiniciar.disabled = false
     spanTurno.textContent = 'O'
+    casillas.forEach(casilla => {
+        casilla.disabled = false
+    })
+})
+inicioX.addEventListener('click', (event) => {
+    numComparar = 10
+    contador = 1
+    inicioO.disabled = true
+    inicioX.disabled = true
+    reiniciar.disabled = false
+    spanTurno.textContent = 'X'
     casillas.forEach(casilla => {
         casilla.disabled = false
     })
@@ -139,16 +145,18 @@ casillas.forEach(casilla => {
         }
         contador++
         if (ganador == true) {
+
             if (event.target.textContent == 'X') {
                 ganados_x++
-
+                spanGandos_x.textContent = ganados_x
             } else {
                 ganados_o++
+                spanGandos_o.textContent = ganados_o
             }
-
             contenedorGanador.className = 'bg-info p-5 mt-5 rounded-5 contenedor-ganador '
             spanGanador.textContent = event.target.textContent
-        } else if (contador == 10) {
+
+        } else if (contador == numComparar) {
             contenedorGanador.className = 'bg-info p-5 mt-5 rounded-5 contenedor-ganador '
             spanGanador.textContent = '¡EMPATE!!'
         }
